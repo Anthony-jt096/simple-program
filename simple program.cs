0e99c0a8d3b961a9d98e86dcbba06305d969d81f
Anthony-jt096 <at1114083@student.francistuttle.edu>
2024-08-22 08:30:23 -0500

    Add files via upload

---
+++ simple program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello World, Press any button to continue");
+
+// Wait for the user to press any key
+Console.ReadKey();
+
+// Print the second message
+Console.WriteLine("Hello again");
